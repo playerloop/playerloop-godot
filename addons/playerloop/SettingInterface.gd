023a45a2476f3e108151e74481a8768813d6f30c
Dark Peace <74102789+rbecker5@users.noreply.github.com>
2022-11-07 21:07:26 +0100

    Interface for authentification

--- addons/playerloop/SettingInterface.gd
+++ addons/playerloop/SettingInterface.gd
@@ -1,9 +1,5 @@
 tool
 extends Control
 
-func _ready() -> void:
-	Playerloop.request.post("test dock")
-	print("hey")
-
 func _on_Submit_pressed() -> void:
 	Playerloop.config["playerloopSecret"] = $Label.text
