d0603634bac5bc645bfb919f58dd19dc4b7f1087
Dark Peace <74102789+rbecker5@users.noreply.github.com>
2022-11-13 11:50:50 +0100

    Config function to set secret

---
+++ addons/playerloop/playerloop.gd
@@ -0,0 +1,26 @@
+class_name PlayerloopSDK
+extends Node
+
+const ENVIRONMENT_VARIABLES : String = "playerloop/config"
+var request : PlayerloopRequest
+var debug: bool = false
+var config : Dictionary = { "playerloopSecret": ""}
+
+func _ready() -> void:
+	load_config()
+
+# Load all config settings from ProjectSettings
+func load_config() -> void:
+	if config.playerloopSecret != "":
+		load_nodes()
+		return
+	printerr("Add your secret in the res://addons/Playerloop/playerloop.gd file, line 13")
+
+func load_nodes() -> void:
+	request = PlayerloopRequest.new()
+	request._secret = config["playerloopSecret"]
+	add_child(request)
+
+func config(secret:String):
+	config["playerloopSecret"] = secret
+	load_nodes()
--- addons/playerloop/plugin.gd
+++ addons/playerloop/plugin.gd
@@ -5,9 +5,6 @@ var interface = preload("res://addons/playerloop/SettingInterface.tscn").instanc
 
 func _enter_tree():
 	add_autoload_singleton("Playerloop", "res://addons/playerloop/Playerloop/playerloop.gd")
-	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, interface)
-
 
 func _exit_tree():
-	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, interface)
 	remove_autoload_singleton("Playerloop")
